20104c77e7e5b34bf1cd2e0e4cf301d13fd6c72e
bnjmn <benja_e_e@hotmail.com>
2023-11-02 22:23:11 -0300

    avanc

---
+++ Retos/FindSum.py
@@ -0,0 +1,21 @@
+# backtrcking
+def find_sum(numeros:list, target:int) -> list:
+    
+
+    def backtracking(combi:list):
+        # solución encontrada
+        return
+
+        # no posee solución
+        if target < 0 or start == len(numbers):
+
+        #backtrack
+        return
+    
+    result = []
+    numeros.sort()
+    backtracking([])
+    return result
+
+
+print(find_sum([1, 5, 3, 2], 6))
