dbb3fafe50ac8c1bd74eaa0183db0affa16ee2e3
bnjmn <benja_e_e@hotmail.com>
2023-11-05 23:39:15 -0300

    add api get, put, delete & post

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 # entidad users
 
 class User(BaseModel):
@@ -23,9 +22,41 @@ async def users():
     return users_list
 
 @app.get("/users/{id}")
-async def user(id: int):
+async def search_user(id: int):
     a = filter(lambda user: user.id == id, users_list)
     try:
         return list(a)[0] 
     except:
         return {"benja": "hola"}
+
+@app.post("/user/")
+async def add_user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"Error": "El usuario existe"}
+    else:
+        users_list.append(user)
+        return user
+
+# path para actualizar una sola variable del usaurio
+# put usuario completo
+
+@app.put("/user/")
+async def user_update(user:User):
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"Error": "El usuario existe"}
+    else:
+        return user
+
+
+@app.delete("/user/{id}")
+async def user_delete(id: int):
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+
