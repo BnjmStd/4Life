789a0e52d7fc8eb84be7459de263f5b59366db45
bnjmn <benja_e_e@hotmail.com>
2023-11-13 19:31:36 -0300

    intentando arreglar la conecc

--- backend/db/client.py
+++ backend/db/client.py
@@ -1,8 +1,19 @@
 
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.getenv("DATABASE_USER")
+pwd = os.getenv("DATABASE_PASSWORD")
+
 
 db_client = MongoClient()
 
+
+
+
 # Configuración de la conexión a MongoDB en Atlas
-mongodb_url = "mongodb+srv://bnjmn:<PzNgmiXFGFnHrZ8e>@cluster0.q04hfcj.mongodb.net/?retryWrites=true&w=majority"
+mongodb_url = "`mongodb+srv://{}:<{}>@cluster0.q04hfcj.mongodb.net/?retryWrites=true&w=majority`".format(user, pwd)
 client = MongoClient(mongodb_url)
