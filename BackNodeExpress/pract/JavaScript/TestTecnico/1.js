1044e70238e68fe37355430716a795e1e67394a8
bnjmn <benja_e_e@hotmail.com>
2024-01-11 00:20:02 -0300

    move carpeta pract

--- BackNodeExpress/pract/JavaScript/TestTecnico/1.js
+++
@@ -1,5 +0,0 @@
-export function obtenerDatosPromesa(callback){
-    setTimeout(() => {
-        callback(null, {data: 'datos'});
-    }, 2000);
-    }
--- BackNodeExpress/pract/TypeScript/CursoIntensivo2/fetch.mts
+++
@@ -1,2 +0,0 @@
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(res => console.log(res))
