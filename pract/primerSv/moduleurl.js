cccb14f597ae2cf116c818092d8eb88dd579dd76
bnjmn <benja_e_e@hotmail.com>
2024-01-12 23:46:33 -0300

    Eliminé la carpeta de pract que era la carpeta que use para aprender js xd

--- pract/primerSv/moduleurl.js
+++
@@ -1,7 +0,0 @@
-const muURL = new URL('https://www.ejemplos.org/cursos/programacion?ordenar=vistas&nivel=1');
-
-console.log(muURL.hostname);
-console.log(muURL.pathname);
-console.log(muURL.searchParams);
-console.log(typeof muURL.searchParams);
-console.log(muURL.searchParams.get('ordenar'));
